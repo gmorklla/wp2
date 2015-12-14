85c3f57653171774ad4c85ac0900f2faff9da83b
gmorklla <gmorklla@gmail.com>
2015-12-14 17:05:44 -0600

    Loaded css and js files inside functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+function wpc_theme_styles() {
+	wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'fonts_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpc_theme_styles' );
+
+function wpc_theme_js() {
+	wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );
+	wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '', true ); // true or false for putting on footer or header of our site
+	wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpc_theme_js');
+
+?>
